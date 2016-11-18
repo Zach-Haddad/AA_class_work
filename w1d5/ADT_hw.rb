5174bb3053c6ab865e7ddd302b11f9e7db2fb65c
Zach Haddad <zh@Zachs-MacBook-Air.local>
2016-11-17 19:28:31 -0800

    testing commit

--- w1d5/ADT_hw.rb
+++ w1d5/ADT_hw.rb
@@ -1,5 +1,25 @@
 # Stack
 
+class Stack
+   def initialize
+     # create ivar to store stack here!
+   end
+
+   def add(el)
+     # adds an element to the stack
+   end
+
+   def remove
+     # removes one element from the stack
+   end
+
+   def show
+     # return a copy of the stack
+   end
+ end
+
 # Queue
 
 # Map
+
+#sample
