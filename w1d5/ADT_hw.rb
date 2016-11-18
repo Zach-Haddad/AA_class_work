a93d09c1775d43f452e346fd433d86b469ba7580
Zach-Haddad <Zhaddad7@gmail.com>
2016-11-17 19:45:40 -0800

    finished stack

--- w1d5/ADT_hw.rb
+++ w1d5/ADT_hw.rb
@@ -1,25 +1,26 @@
-# Stack
+# Stack - write a stack; LIFO
 
 class Stack
    def initialize
-     # create ivar to store stack here!
+     @stack = []
    end
 
    def add(el)
-     # adds an element to the stack
+     @stack << el
    end
 
    def remove
-     # removes one element from the stack
+     @stack.pop
    end
 
    def show
-     # return a copy of the stack
-     
+     @stack
    end
  end
 
-# Queue
+# Queue - FIFO
+class Queue
+end
 
 # Map
 
