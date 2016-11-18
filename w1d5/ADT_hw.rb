f6e3429464c1596ccecd121e03302567dc416709
Zach-Haddad <Zhaddad7@gmail.com>
2016-11-17 20:09:54 -0800

    finished adt

--- w1d5/ADT_hw.rb
+++ w1d5/ADT_hw.rb
@@ -14,14 +14,54 @@ class Stack
    end
 
    def show
-     @stack
+     @stack.dup
    end
  end
 
 # Queue - FIFO
 class Queue
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    @queue << el
+  end
+
+  def dequeue
+    @queue.shift
+  end
+
+  def show
+    @queue.dup
+  end
 end
 
-# Map
+# Map -- using array! ex :  my_map = [[k1, v1], [k2, v2], [k3, v2], ...].
+# instance methods : ssign(key, value), lookup(key), remove(key), show
+# assign method can be used to either create a new key-value pair or update
+# the value for a pre-existing key. It's up to you to check whether a key currently exists!
 
-#sample
+class Map
+  def initialize
+    @map = []
+  end
+
+  def assign(key,value) #assign or update!
+    match_i = @map.find_index{|k,v| key == k}
+    match_i.nil? ? @map << [key,value] : @map[match_i, 1] = value
+  end
+
+  def lookup(key)
+    @map[@map.find_index{|k,v| key == k}][1]
+  end
+
+  def remove(key)
+    @map.delete_at(@map.find_index{|k,v| key == k})
+  end
+
+  def show
+    @map.dup
+  end
+
+end
