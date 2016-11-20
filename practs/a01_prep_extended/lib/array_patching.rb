22e65e72b0c3de56ca3d9c1cab502cb2bc955ce3
Zach-Haddad <Zhaddad7@gmail.com>
2016-11-19 17:35:02 -0800

    finished array_patching

--- practs/a01_prep_extended/lib/array_patching.rb
+++ practs/a01_prep_extended/lib/array_patching.rb
@@ -111,7 +111,21 @@ class Array
   # Write a monkey patch of binary search that accepts a comparison block:
   # E.g. [1, 2, 3, 4, 5, 7].my_bsearch(6) {|el, targ| el+1 <=> targ} => 4
   def my_bsearch(target, &prc)
+    return nil if size == 0
+    prc ||= proc {|a,b| a <=> b}
+    #use dup and take/drop when you don't want to modify original array!!
 
+    midpoint = length/2
+    case prc.call(target, self[midpoint])
+    when -1
+      self.dup.take(midpoint).my_bsearch(target, &prc)
+    when 0
+      return midpoint
+    when 1
+      res = self.dup.drop(midpoint + 1).my_bsearch(target, &prc)
+      res.nil? ? nil : midpoint + 1 + res
+      #need mid + 1 + res; 1 accounts for index offset, res is new midpt, mid is old midpt
+    end
   end
 
 end
