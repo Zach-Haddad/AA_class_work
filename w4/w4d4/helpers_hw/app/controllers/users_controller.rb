dba3dab6255c5f22e0981373d5ee05f488955724
Zach-Haddad <Zhaddad7@gmail.com>
2016-12-07 21:34:12 -0800

    fixed mailers in w4d4 hw

--- w4/w4d4/helpers_hw/app/controllers/users_controller.rb
+++ w4/w4d4/helpers_hw/app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       login_user!(@user)
-      msg = UserMailer.welcome_email
+      msg = UserMailer.welcome_email(@user)
       msg.deliver
       redirect_to cats_url
     else
