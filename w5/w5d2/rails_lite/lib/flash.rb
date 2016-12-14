9e5c743090553d907a1d17bf0b43734cc00bf0a6
Zach-Haddad <Zhaddad7@gmail.com>
2016-12-13 22:36:42 -0800

    began phase 8 of rails lite project

--- w5/w5d2/rails_lite/lib/flash.rb
+++ w5/w5d2/rails_lite/lib/flash.rb
@@ -1,4 +1,23 @@
 require 'json'
 
 class Flash
+  def initialize(req)
+    cookie = req.cookies['_rails_lite_app']
+    @data = cookie ? JSON.parse(cookie) : {}
+  end
+
+  def [](key)
+    @data[key]
+  end
+
+  def []=(key, val)
+    @data[key] = val
+  end
+
+  # serialize the hash into json and save in a cookie
+  # add to the responses cookies
+  def store_flash(res)
+    cookie = { path: '/', value: @data.to_json }
+    res.set_cookie('_rails_lite_app', cookie)
+  end
 end
