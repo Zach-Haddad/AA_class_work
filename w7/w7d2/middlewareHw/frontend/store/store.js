4c1b1a01ed83f40826527883b7ec31eb39e3e010
Zach-Haddad <Zhaddad7@gmail.com>
2017-01-02 13:27:27 -0800

    hwk w7d2

---
+++ w7/w7d2/middlewareHw/frontend/store/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware } from 'redux';
+import RootReducer from '../reducers/root_reducer';
+import addLoggingToDispatch from './../todo_redux';
+
+const configureStore = (preloadedState = {}) => {
+  const store = createStore(RootReducer, preloadedState, applyMiddleware(addLoggingToDispatch));
+  store.subscribe(() => {
+    localStorage.state = JSON.stringify(store.getState());
+  });
+  return store;
+};
+
+export default configureStore;
