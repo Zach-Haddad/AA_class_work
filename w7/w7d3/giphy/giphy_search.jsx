44947f211491d8aad1463031035d3f91e4364dcd
Zach-Haddad <Zhaddad7@gmail.com>
2017-01-04 16:55:51 -0800

    w7d3 hw

--- w7/w7d3/giphy/giphy_search.jsx
+++ w7/w7d3/giphy/giphy_search.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-// import {fetchSearchGiphys} from './util/api_util.js';
+import { fetchSearchGiphys } from './util/api_util';
+import { receiveSearchGiphys } from './actions/giphy_actions';
 
-// window.fetchSearchGiphys = fetchSearchGiphys;
+document.addEventListener('DOMContentLoaded', () => {
+	const store = configureStore();
+	const root = document.getElementById('root');
+	ReactDOM.render(<Root store={store} />, root);
+  // window.store = store;
+  // window.fetchSearchGiphys = fetchSearchGiphys;
+  // window.receiveSearchGiphys = receiveSearchGiphys;
+});
